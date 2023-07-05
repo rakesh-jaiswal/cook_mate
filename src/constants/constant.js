03696b5a1f4571085aa164be42745bb1cdc43c0a
Rakesh Kumar Jaiswal <88997469+rakesh-jaiswal@users.noreply.github.com>
2023-07-05 21:44:38 +0530

    first commit

---
+++ src/constants/constant.js
@@ -0,0 +1,4 @@
+export const logo = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRONg703JJFHv8lx3EvBxtE1DUujhuHEx5khA&usqp=CAU';
+
+
+export const API_URL = 'https://forkify-api.herokuapp.com/api';
